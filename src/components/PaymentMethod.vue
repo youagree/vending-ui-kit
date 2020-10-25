77f0fb6d565843d9ad5b928360f8ff39331fe3b1
Sergey <SergeySm128@gmail.com>
2020-10-25 17:04:43 +0200

    optimize style fo payment

--- src/components/PaymentMethod.vue
+++ src/components/PaymentMethod.vue
@@ -2,14 +2,22 @@
   <div>
     <div class="payment-method">
       <div class="payment-method__col">
-        <button class="button button_payment payment-method__button">Купюрами и монетами</button>
+        <button class="button button_payment payment-method__button"
+                v-bind:class="{ active: this.methodId === 1 }"
+                v-on:click="setPaymentMethod(1)">
+          Купюрами и монетами
+        </button>
         <ul class="payment-method__coins">
           <li><p>Принимаются купюры номиналом в 10 &#8381;, 50 &#8381;, 100 &#8381;, 200 &#8381;.</p></li>
           <li><p>Принимаются монеты номиналом в 1 &#8381;, 2 &#8381;, 5 &#8381;, 10 &#8381;.</p></li>
         </ul>
       </div>
       <div class="payment-method__col">
-        <button class="button button_payment payment-method__button">Бесконтактно</button>
+        <button class="button button_payment payment-method__button"
+                v-bind:class="{ active: this.methodId === 2 }"
+                v-on:click="setPaymentMethod(2)">
+          Бесконтактно
+        </button>
         <p class="payment-method__card">Оплата с помощью карты или смартфона с функцией NFC</p>
         <div class="payment-method__pays">
           <p>Поддерживаемые платежные системы</p>
@@ -30,60 +38,58 @@
 
 <script>
   export default {
-    name: 'PaymentMethod'
+    name: 'PaymentMethod',
+    data: function () {
+      return {
+        methodId: 0
+      }
+    },
+    methods: {
+      setPaymentMethod(methodId = 0){
+        this.methodId = methodId
+      }
+    }
   }
 </script>
 
 <style lang="sass">
 .payment-method
   display: flex
-  .payment-method__col
+.payment-method__col
+  display: flex
+  flex-direction: column
+  flex: 50%
+  padding: 0 5px
+.payment-method__coins
+  padding: 0 15px
+  li
+    border-bottom: 1px solid $g-color-border
+    color: $color-order-card-info
+    padding: 15px 0
+    &:last-child
+      border-bottom: none
+  p
+    color: #000
+
+.payment-method__card
+  padding: 15px 0
+.payment-method__pays
+  color: $color-info-line-text
+  font-size: 12px
+  padding: 5px 0
+  p
+    padding-bottom: 10px
+  ul
+    list-style: none
+
+.payment-method__pay
+  ul
+    display: flex
+  li
     display: flex
     flex-direction: column
-    flex: 50%
-    padding: 0 5px
-  .payment-method__coins
-    padding: 0 15px
-    li
-      border-bottom: 1px solid $g-color-border
-      color: $color-order-card-info
-      padding: 15px 0
-      &:last-child
-        border-bottom: none
-    p
-      color: #000
-
-  .payment-method__card
-    padding: 15px 0
-  .payment-method__pays
-    color: $color-info-line-text
-    font-size: 12px
-    padding: 5px 0
-    p
-      padding-bottom: 10px
-    ul
-      list-style: none
-
-  .payment-method__pay
-    ul
-      display: flex
-    li
-      display: flex
-      flex-direction: column
-      padding: 5px
-    img
-      height: 20px
-      max-width: 100%
-  .button_payment
-    align-items: center
-    background-color: #fff
-    border: 1px solid $g-color-border
-    border-radius: 8px
-    color: $g-color-accent
-    display: inline-flex
-    justify-content: center
-    margin: 20px 0
-    padding: 15px
-    text-transform: uppercase
-    width: 90%
+    padding: 5px
+  img
+    height: 20px
+    max-width: 100%
 </style>
