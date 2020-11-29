d7e68deefbce8e8cf1794cb98bebb3c56ce00693
Sergey <SergeySm128@gmail.com>
2020-11-29 11:44:43 +0200

    add end step

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,33 +1,43 @@
 <template>
   <div class="order">
-    <div class="order__header">
-      <router-link class="order__back" to="/">
-        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512.006 512.006">
-          <path d="M388.419,475.59L168.834,256.005L388.418,36.421c8.341-8.341,8.341-21.824,0-30.165s-21.824-8.341-30.165,0    L123.586,240.923c-8.341,8.341-8.341,21.824,0,30.165l234.667,234.667c4.16,4.16,9.621,6.251,15.083,6.251    c5.461,0,10.923-2.091,15.083-6.251C396.76,497.414,396.76,483.931,388.419,475.59z"/>
-        </svg>
-        <span>На главный экран</span>
-      </router-link>
-      <OrderCard :product="product" class="order__card" />
+    <div v-if="stepStates[4] === 0">
+      <div class="order__header">
+        <router-link class="order__back" to="/">
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512.006 512.006">
+            <path d="M388.419,475.59L168.834,256.005L388.418,36.421c8.341-8.341,8.341-21.824,0-30.165s-21.824-8.341-30.165,0    L123.586,240.923c-8.341,8.341-8.341,21.824,0,30.165l234.667,234.667c4.16,4.16,9.621,6.251,15.083,6.251    c5.461,0,10.923-2.091,15.083-6.251C396.76,497.414,396.76,483.931,388.419,475.59z"/>
+          </svg>
+          <span>На главный экран</span>
+        </router-link>
+        <OrderCard :product="product" class="order__card" />
+      </div>
+      <div class="order__main">
+        <div class="order__box" v-bind:class="getBoxClass(1)">
+          <OrderBoxHeader step="1" label="Выберите способ оплаты" />
+          <div class="order__box-content"  v-if="stepStates[1]">
+            <PaymentMethod @changePaymentMethod="changePaymentMethod" @setThirdStep="setThirdStep" />
+          </div>
+        </div>
+        <div class="order__box" v-bind:class="getBoxClass(2)">
+          <OrderBoxHeader step="2" label="Оплата" />
+          <div class="order__box-content" v-if="stepStates[2]">
+            <Payment v-bind:paymentMethod="paymentMethod" v-bind:sum="product.price" />
+          </div>
+        </div>
+        <div class="order__box" v-bind:class="getBoxClass(3)">
+          <OrderBoxHeader step="3" label="Получение товара" />
+          <div class="order__box-content"  v-if="stepStates[3]">
+            <ReceiptGood @eventEndOrder="eventEndOrder" />
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="order__main">
-      <div class="order__box" v-bind:class="getBoxClass(1)">
-        <OrderBoxHeader step="1" label="Выберите способ оплаты" />
-        <div class="order__box-content"  v-if="stepStates[1]">
-          <PaymentMethod @changePaymentMethod="changePaymentMethod" @setThirdStep="setThirdStep" />
-        </div>
+    <div v-else class="end-step">
+      <img src="/icon-success.png" alt="success order">
+      <div v-if="getCashBack()" class="cash-back">
+        Ваша сдача: {{getCashBack()}} ₽
       </div>
-      <div class="order__box" v-bind:class="getBoxClass(2)">
-        <OrderBoxHeader step="2" label="Оплата" />
-        <div class="order__box-content" v-if="stepStates[2]">
-          <Payment v-bind:paymentMethod="paymentMethod" v-bind:sum="product.price" />
-        </div>
-      </div>
-      <div class="order__box" v-bind:class="getBoxClass(3)">
-        <OrderBoxHeader step="3" label="Получение товара" />
-        <div class="order__box-content"  v-if="stepStates[3]">
-          <ReceiptGood @eventEndOrder="eventEndOrder" />
-        </div>
-      </div>
+      <h1>Спасибо за покупку. Заберите товар в нижнем окне</h1>
+      <button @click="$router.push('/')" class="button button_accent">На главный экран</button>
     </div>
   </div>
 </template>
@@ -44,7 +54,7 @@
     data: function() {
       return {
         paymentMethod: 0,
-        stepStates: {1:1,2:0,3:0}
+        stepStates: {1:1,2:0,3:0,4:0}
       }
     },
     methods: {
@@ -61,9 +71,11 @@
       changePaymentMethod(methodId = 0){
         this.paymentMethod = methodId
         if(methodId) {
-          this.stepStates = {1:2,2:1,3:0}
+          this.stepStates[1] = 2
+          this.stepStates[2] = 1
         } else {
-          this.stepStates = {1:1,2:0,3:0}
+          this.stepStates[1] = 1
+          this.stepStates[2] = 0
         }
       },
       setThirdStep() {
@@ -72,10 +84,14 @@
       },
       eventEndOrder() {
         console.log('end order')
+        this.stepStates[4] = 2
+      },
+      getCashBack() {
+        return this.incomeSum - this.product.price
       }
     },
     computed: {
-      ...mapGetters(['product'])
+      ...mapGetters(['product','incomeSum'])
     }
   }
 </script>
@@ -134,4 +150,28 @@
           width: 2px
           height: 100%
           border-left: 2px dotted $color-order-step
+  .end-step
+    text-align: center
+    padding: 100px 50px
+    img
+      display: block
+      margin: 50px auto
+    .cash-back
+      background-color: #f5f5f5
+      border: 1px solid $g-color-border
+      border-radius: 5px
+      bottom: -25px
+      margin: auto
+      padding: 12px 30px
+      position: relative
+      width: fit-content
+    h1
+      border-bottom: 1px solid $g-color-border
+      border-top: 1px solid $g-color-border
+      font-weight: 500
+      margin-bottom: 100px
+      padding: 50px 100px
+    button
+      font-size: 18px
+      padding: 15px 30px
 </style>
